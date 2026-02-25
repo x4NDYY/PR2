66cc40e3ee44cf57b3ecfbedce8d100edd2d83eb
x4NDYY <northburan@inbox.ru>
2026-02-26 02:15:17 +0300

    Initial PR2 structure

---
+++ src/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку (не более 20 символов): ");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Строчка пустая! Повторите попытку!");
+            return;
+        }
+
+        if (input.Length > 20)
+        {
+            Console.WriteLine("Слишком длинная строка! Повторите попытку!");
+            return;
+        }
+
+        char symbol = '\0';
+        int maxCountSymbol = 0;
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            char current = input[i];
+            int count = 0;
+
+            if(current == ' ' )
+                continue;
+
+            for(int j = 0; j < input.Length; j++)
+            {
+                if (input[j] == current)
+                    count++;
+            }
+
+            if(maxCountSymbol < count)
+            {
+                maxCountSymbol = count;
+                symbol = current;
+            }
+        }
+
+        if (symbol == '\0')
+        {
+            Console.WriteLine("В строке только пробелы.");
+            return;
+        }
+
+        string result = input.Replace(' ', symbol);
+
+        Console.WriteLine(result);
+    }
+}
